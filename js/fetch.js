2985ff43c9fa922b67776b11ec8e05d6a2741374
Laura Lopez <lauraamelina@gmail.com>
2021-09-24 21:15:32 -0300

    Funcion madeGrid
    Creamos la funcion made grid que recibe el Json, para mostrar toda la informacion deseada.
    
    Agregamos una prueba de error 404 por si algun elemento no se encuentra.

--- js/fetch.js
+++ js/fetch.js
@@ -23,3 +23,33 @@ function buscarEnApi(cuidad) {
             console.log('Algo fallo!', error);
         });
 }
+
+
+function madeGrid(data) {
+    console.log('Data:', data);
+    if (data.cod == 404) {
+        console.log('ERROR 404! POR FAVOR, INGRESAR UNA CUIDAD VALIDA');
+    } else {
+        let maxima = document.createElement("p");
+        let minima = document.createElement("p");
+        let humedad = document.createElement("p");
+        let sensacion = document.createElement("p");
+        let presion = document.createElement("p");
+        let viento = document.createElement("p");
+
+        main.appendChild(maxima);
+        main.appendChild(minima);
+        main.appendChild(humedad);
+        main.appendChild(sensacion);
+        main.appendChild(presion);
+        main.appendChild(viento);
+
+        maxima.innerHTML = 'Maxima: ' + data.main.temp_max;
+        minima.innerHTML = 'Minima: ' + data.main.temp_min;
+        humedad.innerHTML = 'Humedad: ' + data.main.humidity;
+        sensacion.innerHTML = 'Sensación Térmica:  ' + data.main.feels_like;
+        presion.innerHTML = 'Presión: ' + data.main.pressure;
+        viento.innerHTML = 'Velocidad del viento: ' + data.wind.speed;
+
+    }
+}
